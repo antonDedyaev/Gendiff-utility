107417e19404da44eec7e4dcc1c212e800af9f42
Aintdead86 <96647913+Aintdead86@users.noreply.github.com>
2022-04-19 19:00:48 +0300

    add gendiff.js to bin

---
+++ bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number');
+
+program.parse();
