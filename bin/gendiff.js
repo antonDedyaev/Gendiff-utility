83b7ace7e0e65be7879def60501dd7b4492f4371
Aintdead86 <96647913+Aintdead86@users.noreply.github.com>
2022-04-20 00:03:20 +0300

    add arguments and output format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ import { program } from 'commander';
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0', '-V, --version', 'output the version number');
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
