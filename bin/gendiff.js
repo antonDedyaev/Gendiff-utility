09da705a2bcd45d1f7ebdc174b2393a8a4144a7e
Aintdead86 <96647913+Aintdead86@users.noreply.github.com>
2022-04-22 15:46:30 +0300

    add action for genDiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
-
+/* eslint-disable import/extensions */
+/* eslint-disable no-console */
 import { program } from 'commander';
+import genDiff from '../src/diffFunc.js';
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
   .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  })
   .option('-f, --format <type>', 'output format');
 
 program.parse();
