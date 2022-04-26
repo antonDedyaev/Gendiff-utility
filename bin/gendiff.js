25a54cc0d60d4ef66bf035a21e9562854d23564e
Aintdead86 <96647913+Aintdead86@users.noreply.github.com>
2022-04-26 10:40:22 +0300

    correct import path

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
-/* eslint-disable import/extensions */
-/* eslint-disable no-console */
+
 import { program } from 'commander';
-import genDiff from '../src/diffFunc.js';
+import { genDiff } from '../src/diffFunc.js';
 
 program
   .name('gendiff')
