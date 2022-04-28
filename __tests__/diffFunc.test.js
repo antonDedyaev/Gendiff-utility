90758b054ba1a8aedc7173cb1415cd519f10441f
Aintdead86 <96647913+Aintdead86@users.noreply.github.com>
2022-04-28 08:38:50 +0300

    add test for flat yaml/yml

--- __tests__/diffFunc.test.js
+++ __tests__/diffFunc.test.js
@@ -1,7 +1,13 @@
 import { test, expect } from '@jest/globals';
-import { genDiff, readFile } from '../src/diffFunc.js';
+import genDiff from '../src/diffFunc.js';
+import { readFile } from '../utils/parsers.js';
 
-test('correct functioning', () => {
-  const expected = readFile('expectedResult.txt');
-  expect(genDiff('file1.json', 'file2.json')).toEqual(expected);
+const expectedFlat = readFile('referenceFlats.txt');
+
+test('compare flat JSON files', () => {
+  expect(genDiff('file1.json', 'file2.json')).toEqual(expectedFlat);
+});
+
+test('compare flat YAML files', () => {
+  expect(genDiff('file1.yml', 'file2.yaml')).toEqual(expectedFlat);
 });
