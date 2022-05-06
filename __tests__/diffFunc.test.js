6e952f03253033c5d1feb9b76470602ca0268a01
Aintdead86 <96647913+Aintdead86@users.noreply.github.com>
2022-05-06 22:26:09 +0300

    add tests for nested files

--- __tests__/diffFunc.test.js
+++ __tests__/diffFunc.test.js
@@ -2,12 +2,12 @@ import { test, expect } from '@jest/globals';
 import genDiff from '../src/diffFunc.js';
 import { readFile } from '../utils/parsers.js';
 
-const expectedFlat = readFile('referenceFlats.txt');
+const expectedNested = readFile('referenceNested.txt');
 
-test('compare flat JSON files', () => {
-  expect(genDiff('file1.json', 'file2.json')).toEqual(expectedFlat);
+test('compare nested JSON files', () => {
+  expect(genDiff('file1.json', 'file2.json')).toEqual(expectedNested);
 });
 
-test('compare flat YAML files', () => {
-  expect(genDiff('file1.yml', 'file2.yaml')).toEqual(expectedFlat);
+test('compare nested YAML files', () => {
+  expect(genDiff('file1.yml', 'file2.yaml')).toEqual(expectedNested);
 });
