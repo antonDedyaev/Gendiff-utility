47cbe16c8905b9ce0d9acea26a721f62ef346856
Aintdead86 <96647913+Aintdead86@users.noreply.github.com>
2022-05-06 22:27:25 +0300

    split genDiff and formatter functions

--- src/diffFunc.js
+++ src/diffFunc.js
@@ -1,24 +1,33 @@
 import _ from 'lodash';
 import { parse } from '../utils/parsers.js';
+import stylish from '../formatters/stylish.js';
 
 const genDiff = (filename1, filename2) => {
   const objFromFile1 = parse(filename1);
   const objFromFile2 = parse(filename2);
-  const keysOfObj1 = Object.keys(objFromFile1);
-  const keysOfObj2 = Object.keys(objFromFile2);
-  const allKeys = _.sortBy(keysOfObj1.concat(keysOfObj2));
-  const difference = _.sortedUniq(allKeys)
-    .reduce((acc, el) => {
-      if (!keysOfObj2.includes(el)) {
-        return [...acc, `  - ${el}: ${objFromFile1[el]}`];
-      }
-      if (!keysOfObj1.includes(el)) {
-        return [...acc, `  + ${el}: ${objFromFile2[el]}`];
-      }
-      return objFromFile1[el] === objFromFile2[el]
-        ? [...acc, `    ${el}: ${objFromFile1[el]}`]
-        : [...acc, `  - ${el}: ${objFromFile1[el]}`, `  + ${el}: ${objFromFile2[el]}`];
-    }, []);
-  return `{\n${difference.join('\n')}\n}`;
+
+  const buildTree = (obj1, obj2) => {
+    const keysOfObj1 = Object.keys(obj1);
+    const keysOfObj2 = Object.keys(obj2);
+    const allKeys = _.sortBy(keysOfObj1.concat(keysOfObj2));
+    const difference = _.sortedUniq(allKeys)
+      .map((key) => {
+        if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+          return { key, type: 'nested', children: buildTree(obj1[key], obj2[key]) };
+        } if (!Object.hasOwn(obj1, key)) {
+          return { key, type: 'added', value: obj2[key] };
+        } if (!Object.hasOwn(obj2, key)) {
+          return { key, type: 'deleted', value: obj1[key] };
+        } if (obj1[key] === obj2[key]) {
+          return { key, type: 'unchanged', value: obj1[key] };
+        }
+        return {
+          key, type: 'changed', valBefore: obj1[key], valAfter: obj2[key],
+        };
+      });
+    return difference;
+  };
+  const tree = buildTree(objFromFile1, objFromFile2);
+  return stylish(tree);
 };
 export default genDiff;
