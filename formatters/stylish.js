72b750c26cd86cf52bd2393ed35efc9dcb83886e
Aintdead86 <96647913+Aintdead86@users.noreply.github.com>
2022-05-08 23:29:28 +0300

    add constants and edit code

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -1,43 +1,44 @@
 import _ from 'lodash';
 
-const setIndent = (depth = 1) => {
-  const replacer = ' ';
-  const initialIndent = 2;
-  return replacer.repeat(initialIndent * depth);
-};
+const initialIndent = 2;
+const spacer = ' ';
+const spacesIncrement = 4;
 
-const stringify = (data, depth = 1) => {
+const keyIndent = (depth) => spacer.repeat(initialIndent + (depth * spacesIncrement));
+const bracketIndent = (depth) => spacer.repeat(depth * spacesIncrement);
+
+const stringify = (data, depth) => {
   if (!_.isObject(data)) {
     return `${data}`;
   }
   const result = Object
     .entries(data)
-    .map(([key, value]) => (`${setIndent(depth)}${key}: ${stringify(value, depth + 1)}`));
-  return `{\n${result.join('\n')}\n${setIndent(depth - 1)}}`;
+    .map(([key, value]) => (`${keyIndent(depth + 1)}  ${key}: ${stringify(value, depth + 1)}`));
+  return `{\n${result.join('\n')}\n${bracketIndent(depth + 1)}}`;
 };
 
 const stylish = (tree) => {
   const format = (node, depth) => {
-    const result = node.map(({
+    const setFormat = node.map(({
       key, type, children, value, valBefore, valAfter,
     }) => {
       switch (type) {
         case 'nested':
-          return `${setIndent(depth)}  ${key}: ${format(children, depth + 1)}`;
+          return `${keyIndent(depth)}  ${key}: ${format(children, depth + 1)}`;
         case 'added':
-          return `${setIndent(depth)}+ ${key}: ${stringify(value, depth)}`;
+          return `${keyIndent(depth)}+ ${key}: ${stringify(value, depth)}`;
         case 'deleted':
-          return `${setIndent(depth)}- ${key}: ${stringify(value, depth)}`;
+          return `${keyIndent(depth)}- ${key}: ${stringify(value, depth)}`;
         case 'unchanged':
-          return `${setIndent(depth)}  ${key}: ${stringify(value, depth)}`;
+          return `${keyIndent(depth)}  ${key}: ${stringify(value, depth)}`;
         case 'changed':
-          return `${setIndent(depth)}- ${key}: ${stringify(valBefore, depth)}\n${setIndent(depth)}+ ${key}: ${stringify(valAfter, depth)}`;
+          return `${keyIndent(depth)}- ${key}: ${stringify(valBefore, depth)}\n${keyIndent(depth)}+ ${key}: ${stringify(valAfter, depth)}`;
         default:
           return null;
       }
     });
-    return `{\n${result.join('\n')}\n${setIndent(depth - 1)}}`;
+    return `{\n${setFormat.join('\n')}\n${bracketIndent(depth)}}`;
   };
-  return format(tree, 1);
+  return format(tree, 0);
 };
 export default stylish;
