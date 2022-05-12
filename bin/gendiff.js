8564841c3d7fde982e673dc9f9ca0eac7ecb64d9
Aintdead86 <96647913+Aintdead86@users.noreply.github.com>
2022-05-12 14:33:24 +0300

    add 3d parameter for genDiff in .action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,7 @@ program
   .version('1.0.0', '-V, --version', 'output the version number')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   })
   .option('-f, --format <type>', 'output format', 'stylish');
 
