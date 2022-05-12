bea08c3ad02f7b4a06007886a0b1c8077b6be464
Aintdead86 <96647913+Aintdead86@users.noreply.github.com>
2022-05-12 14:29:14 +0300

    add plain formatter logic

---
+++ formatters/plain.js
@@ -0,0 +1,34 @@
+import _ from 'lodash';
+
+const valueFormat = (value) => {
+  if (_.isString(value)) {
+    return `'${value}'`;
+  }
+  return ((_.isObject(value)) ? '[complex value]' : value);
+};
+
+const plain = (tree) => {
+  const pathToRoot = (node, property = '') => {
+    const setFormat = node.flatMap(({
+      key, type, children, value, valBefore, valAfter,
+    }) => {
+      switch (type) {
+        case 'nested':
+          return pathToRoot(children, `${property}${key}.`);
+        case 'added':
+          return `Property '${property}${key}' was added with value: ${valueFormat(value)}`;
+        case 'deleted':
+          return `Property '${property}${key}' was removed`;
+        case 'changed':
+          return `Property '${property}${key}' was updated. From ${valueFormat(valBefore)} to ${valueFormat(valAfter)}`;
+        case 'unchanged':
+          return [];
+        default:
+          return null;
+      }
+    });
+    return `${setFormat.join('\n')}`;
+  };
+  return pathToRoot(tree);
+};
+export default plain;
